cb4d46e6a490f1ead4e56d68d32eae1c4b22d90b
Ian <mus5002@aol.com>
2018-03-21 10:43:32 -0400

    file changes

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const db = require('./db')
-
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+
+mongoose.connect('mongodb://localhost/polling')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors())
 
 app.use('/', require('./routes'))
-
-//module.exports = app
 
 const port = process.env.PORT || 3030
 app.listen(port, () => {
