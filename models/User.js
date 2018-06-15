7b42c77c8ae55b02abb613b0b8a006276bd58233
Ian <mus5002@aol.com>
2018-06-15 03:15:05 -0400

    added bcrypt-nodejs for password encryption to the user model

--- models/User.js
+++ models/User.js
@@ -1,8 +1,32 @@
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-    username: String,
+    username: { type: String, unique: true },
     password: String
 })
+
+userSchema.pre('save', function(next) {
+
+  const user = this
+
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) { return next(err) }
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) { return next(err) }
+
+      user.password = hash
+      next()
+    })
+  })
+})
+
+userSchema.methods.comparePassword = function(candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if(err) { return callback(err) }
+
+    callback(null, isMatch)
+  })
+}
 
 module.exports = mongoose.model('User', UserSchema)
